94b428523942345149799012b4b1128b1c616253
Lívia Keller <101295392+kellerlivia@users.noreply.github.com>
2022-09-04 17:59:32 -0300

    Challenge
    UIPickerView Functionality
    Update the User Interface Using the Delegate Pattern
    API used: https://www.coinapi.io/

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Livia Carvalho Keller on 04/09/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
